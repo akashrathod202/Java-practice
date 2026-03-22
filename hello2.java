49fc44493e92f49f44b6528362b1c6cf77b7b879
Akash Rathod <akbhau2004@gmail.com>
2026-03-23 00:12:02 +0530

    solve the question of method overloading

--- hello2.java
+++ hello2.java
@@ -10,18 +10,37 @@
 // }
 
 
-class hello2{
+// class hello2{
 
-     static  void  eo(int a){
-        if(a %  2 == 0){
-            System.out.println("this is even number");
-        }else{
-            System.out.println("this is not a even number");
-        }
+//      static  void  eo(int a){
+//         if(a %  2 == 0){
+//             System.out.println("this is even number");
+//         }else{
+//             System.out.println("this is not a even number");
+//         }
      
 
+//     }
+//     public static void main(String[] args){
+//       eo(7);
+//     }
+// }
+
+
+// method overloading
+
+class hello2{
+    static int add(int a,int b){
+        return a + b;
     }
+
+    static int add(int a, int b, int c){
+        return a + b + c;
+
+    }
+
     public static void main(String[] args){
-      eo(7);
-    }
+        System.out.println(add(2, 3));      
+        System.out.println(add(2, 3, 4));
+    } 
 }
